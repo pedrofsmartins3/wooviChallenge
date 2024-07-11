2d67427729607a955273f6acd18037193099e37f
PEDRO_MARTINS <pedrofsmartins3@gmail.com>
2024-07-11 17:10:29 +0100

    vite-config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "Curriculum-react-project-",
+  base: "woovichallenge",
   plugins: [react()],
 });
